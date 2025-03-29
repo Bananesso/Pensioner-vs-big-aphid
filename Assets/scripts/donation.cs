30d37e7007e09af5f717615c730c83f11499bfb2
sexBOMBA19 <gonder.barsik@gmail.com>
2025-03-29 16:12:21 +0400

    Menu hyper

---
+++ Assets/scripts/donation.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class OpenWebsite : MonoBehaviour
+{
+    public string url = "https://www.donationalerts.com/r/boomblasta"; 
+
+    void Start()
+    {
+        
+        Button button = GetComponent<Button>();
+        if (button != null)
+        {
+            button.onClick.AddListener(OpenURL);
+        }
+    }
+
+    public void OpenURL()
+    {
+        Application.OpenURL(url);
+        Debug.Log("Opening URL: " + url);
+    }
+}
