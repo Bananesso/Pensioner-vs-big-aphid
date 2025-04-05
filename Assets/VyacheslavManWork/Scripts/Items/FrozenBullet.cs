0da16034bdbb342cbb83d9d10eefd11a2b3aa278
Bananesso <shamangamer999@gmail.com>
2025-04-05 18:23:05 +0300

    не робит

---
+++ Assets/VyacheslavManWork/Scripts/Items/FrozenBullet.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using UnityEngine;
+
+public class FrozenBullet : MonoBehaviour
+{
+    private InteractionController controller;
+    public float Damage;
+    private void Awake()
+    {
+        Destroy(gameObject, 7);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Health health = other.GetComponent<Health>();
+        if (health != null)
+        {
+            health.TakeDamage(Damage);
+            StartCoroutine(EnemyFreeze(other.GetComponent<EnemyAI>()));
+        }
+    }
+    IEnumerator EnemyFreeze(EnemyAI enemy)
+    {
+        enemy.enabled = false;
+        yield return new WaitForSeconds(2);
+        enemy.enabled = true;
+    }
+}
