cc792ce17b2ce21d66ef2c13c01a86c4cc728e17
Bananesso <shamangamer999@gmail.com>
2025-04-30 14:25:31 +0400

    fgfg

--- Assets/VyacheslavManWork/Scripts/RandomErrorLoadScene.cs
+++ Assets/VyacheslavManWork/Scripts/RandomErrorLoadScene.cs
@@ -6,19 +6,17 @@ public class RandomErrorLoadScene : MonoBehaviour
     [SerializeField] private string _errorSceneName;
     [SerializeField] private string _normalSceneName;
 
-    [SerializeField] private int _minRandomValue;
-    [SerializeField] private int _maxRandomValue;
-    [SerializeField] private int _needValue;
-    private float _randomValue;
+    [SerializeField] private int _precentToLoadErr;
+    private int _randomValue;
 
     private void Start()
     {
-        _randomValue = Random.Range(_minRandomValue, _maxRandomValue);
+        _randomValue = Random.Range(0, 100);
     }
 
     public void LoadErrorSceneByName(string sceneName)
     {
-        if (_randomValue == _needValue)
+        if (_randomValue < _precentToLoadErr)
         {
             SceneManager.LoadScene(_errorSceneName);
         }
