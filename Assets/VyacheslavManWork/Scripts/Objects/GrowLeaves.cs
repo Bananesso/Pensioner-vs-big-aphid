7f686607a2a3b71b10b89ce51bd980b5dbfb3e0a
Bananesso <shamangamer999@gmail.com>
2025-08-11 18:22:26 +0400

    fgfg

--- Assets/VyacheslavManWork/Scripts/Objects/GrowLeaves.cs
+++ Assets/VyacheslavManWork/Scripts/Objects/GrowLeaves.cs
@@ -6,22 +6,25 @@ public class GrowLeaves : MonoBehaviour, IInteractWithObj
     [Header("Настройки")]
     [SerializeField] private float _growTime;
     [SerializeField] private int _leavesLoot;
-    [SerializeField] private GameObject _leavesVisual;
+    [SerializeField] private int _materialsLoot;
+    private bool _growed;
 
     private ListikiPodschet _listikiPodschet;
+    private AnimationLogic _growAnimation;
 
     void Start()
     {
+        _growAnimation = GetComponent<AnimationLogic>();
         _listikiPodschet = FindObjectOfType<ListikiPodschet>();
         StartCoroutine(Grow());
     }
 
     public void Interact()
     {
-        if (_leavesVisual.activeSelf)
+        if (_growed)
         {
-            _leavesVisual.SetActive(false);
             _listikiPodschet.KolichestvoListikov += _leavesLoot;
+            _listikiPodschet.KolichestvoMaterialov += _materialsLoot;
             StartCoroutine(Grow());
         }
         else return;
@@ -29,7 +32,8 @@ public class GrowLeaves : MonoBehaviour, IInteractWithObj
 
     private IEnumerator Grow()
     {
+        _growAnimation.PlayAttackAnimation();
         yield return new WaitForSeconds(_growTime);
-        _leavesVisual.SetActive(true);
+        _growed = true;
     }
 }
