576492784d991adb6397bff5227b8840ec78de39
Bananesso <shamangamer999@gmail.com>
2025-10-18 23:19:36 +0400

    а

--- Assets/VyacheslavManWork/Scripts/Objects/Sintezator.cs
+++ Assets/VyacheslavManWork/Scripts/Objects/Sintezator.cs
@@ -1,40 +1,46 @@
-using Cinemachine;
 using UnityEngine;
+using Cinemachine;
 
 public class Sintezator : MonoBehaviour, IInteractWithObj
 {
     [SerializeField] private GameObject _magaz;
-    [SerializeField] private CinemachinePOV _fpCamera;
+    [SerializeField] private CinemachineVirtualCamera _virtualCamera;
     [SerializeField] private PlayerController _player;
 
     private float _verticalAxisVal;
     private float _horisontalAxisVal;
+    private CinemachinePOV _povComponent;
+
+    private void Start()
+    {
+        _povComponent = _virtualCamera.GetCinemachineComponent<CinemachinePOV>();
+    }
 
     public void Interact()
     {
-		_verticalAxisVal = _fpCamera.m_VerticalAxis.Value;
-        _horisontalAxisVal = _fpCamera.m_HorizontalAxis.Value;
+        _verticalAxisVal = _povComponent.m_VerticalAxis.Value;
+        _horisontalAxisVal = _povComponent.m_HorizontalAxis.Value;
 
-		_magaz.SetActive(true);
+        _magaz.SetActive(true);
         _player.enabled = false;
 
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
 
-        _fpCamera.gameObject.SetActive(false);
+        _virtualCamera.gameObject.SetActive(false);
     }
 
     public void CloseMenu()
     {
-        _fpCamera.m_VerticalAxis.Value = _verticalAxisVal;
-        _fpCamera.m_HorizontalAxis.Value = _horisontalAxisVal;
+        _povComponent.m_VerticalAxis.Value = _verticalAxisVal;
+        _povComponent.m_HorizontalAxis.Value = _horisontalAxisVal;
 
-		_magaz.SetActive(false);
+        _magaz.SetActive(false);
         _player.enabled = true;
 
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
 
-        _fpCamera.gameObject.SetActive(true);
+        _virtualCamera.gameObject.SetActive(true);
     }
 }
