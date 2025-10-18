1ad1087497b664624f62c99cc0b14770837809ee
Bananesso <shamangamer999@gmail.com>
2025-10-18 18:12:22 +0300

    jopich

--- Assets/VyacheslavManWork/Scripts/Objects/Sintezator.cs
+++ Assets/VyacheslavManWork/Scripts/Objects/Sintezator.cs
@@ -1,29 +1,40 @@
+using Cinemachine;
 using UnityEngine;
 
 public class Sintezator : MonoBehaviour, IInteractWithObj
 {
     [SerializeField] private GameObject _magaz;
-    [SerializeField] private GameObject _fpCamera;
+    [SerializeField] private CinemachinePOV _fpCamera;
     [SerializeField] private PlayerController _player;
+
+    private float _verticalAxisVal;
+    private float _horisontalAxisVal;
+
     public void Interact()
     {
-        _magaz.SetActive(true);
+		_verticalAxisVal = _fpCamera.m_VerticalAxis.Value;
+        _horisontalAxisVal = _fpCamera.m_HorizontalAxis.Value;
+
+		_magaz.SetActive(true);
         _player.enabled = false;
 
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
 
-        _fpCamera.SetActive(false);
+        _fpCamera.gameObject.SetActive(false);
     }
 
     public void CloseMenu()
     {
-        _magaz.SetActive(false);
+        _fpCamera.m_VerticalAxis.Value = _verticalAxisVal;
+        _fpCamera.m_HorizontalAxis.Value = _horisontalAxisVal;
+
+		_magaz.SetActive(false);
         _player.enabled = true;
 
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
 
-        _fpCamera.SetActive(true);
+        _fpCamera.gameObject.SetActive(true);
     }
 }
